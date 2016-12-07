8c808b8a5cf08b93019b150331e1299b6ab575b7
macinfinity <chriswmaki@gmail.com>
2016-12-06 21:29:52 -0800

    First working parser and ics content creation

--- src/main/java/com/cmcllc/parse/CalendarEventUtil.java
+++ src/main/java/com/cmcllc/parse/CalendarEventUtil.java
@@ -8,7 +8,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Optional;
-import java.util.UUID;
 
 import static com.cmcllc.util.DateTimeUtils.toiCalDate;
 import static com.cmcllc.util.DateTimeUtils.toiCalDateTimeOptional;
