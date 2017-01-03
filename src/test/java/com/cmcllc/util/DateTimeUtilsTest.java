137472a82674c0058ffc16dfea96fcb3ce929e40
macinfinity <chriswmaki@gmail.com>
2017-01-02 21:06:15 -0800

    added Google App Engine

--- src/test/java/com/cmcllc/util/DateTimeUtilsTest.java
+++ src/test/java/com/cmcllc/util/DateTimeUtilsTest.java
@@ -3,6 +3,7 @@ package com.cmcllc.util;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -46,5 +47,15 @@ public class DateTimeUtilsTest {
   public void toiCalDateTime2() throws Exception {
     net.fortuna.ical4j.model.Date iCalDate = DateTimeUtils.toiCalDate(localDateTime);
     assertThat(dateTimeFormat.format(iCalDate), is ("20161204T131500"));
+  }
+
+  @Test
+  public void testZeroHour() throws ParseException {
+//    LocalDateTime dateTime = LocalDateTime.of(LocalDate.of(2016, 12, 4),
+//        LocalTime.of(13, 15, 0,0));
+    LocalDateTime dateTime = LocalDateTime.of(LocalDate.of(2017, 1, 3),
+        LocalTime.of(6, 0, 0, 0));
+    DateTimeUtils.toiCalDate(dateTime);
+//    DateTimeUtils.toiCalDate(localDateTime);
   }
 }
