9c0194ac6c751b1d820c5c23f23b117ff0379b89
macinfinity <chriswmaki@gmail.com>
2017-01-07 21:14:24 -0800

    logging and configuration cleanup

--- src/test/java/com/cmcllc/CalendarMonsterApplicationTests.java
+++ src/test/java/com/cmcllc/CalendarMonsterApplicationTests.java
@@ -5,10 +5,20 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+/**
+ * There is no active profile specified so the 'default' profile will be active.
+ * Spring will set the default active profile to 'prod' but we have an applications.yaml file
+ * that sets the default profile to 'dev'
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CalendarMonsterApplicationTests {
 
+  /**
+   * Run this test and look in the console to see how Spring is configured and which
+   * Beans, etc. thinks it has (if your classes are missing they may not be configured
+   * correctly).
+   */
   @Test
   public void contextLoads() {
   }
